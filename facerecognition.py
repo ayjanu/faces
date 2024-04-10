541d37ef3d7be2cdb5db6939efd00fc4a91c4d38
ayjanu <Ayansjannu@gmail.com>
2024-04-09 19:42:20 -0500

    col

--- facerecognition.py
+++ facerecognition.py
@@ -15,14 +15,20 @@ while True:
     # Detect faces in the frame
     faces = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml').detectMultiScale(frame, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30), flags=cv2.CASCADE_SCALE_IMAGE)
 
+    cols = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 0, 255), (255, 255, 0), (0,255,255)]
+    ems = ["angry", "fear", "neutral", "disgust", "sad","happy","surprised"]
+
     # For each detected face, predict emotion
     for (x, y, w, h) in faces:
         face = frame[y:y+h, x:x+w]
         result = DeepFace.analyze(face, actions=['emotion'], enforce_detection=False)
-
+        col = (0,0,0)
         if isinstance(result, list) and len(result) > 0:
             emotion = max(result[0]['emotion'].items(), key=lambda x: x[1])[0]
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+            for em in len(ems):
+                if emotion == ems[em]:
+                    col = cols[em]
+            cv2.rectangle(frame, (x, y), (x+w, y+h), col, 2)
             cv2.putText(frame, emotion, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
         else:
             print("Face not detected or error in analysis:", result)
