766eb18e88287f49d3b88217c07aefca8307ff05
ayjanu <Ayansjannu@gmail.com>
2024-04-22 14:02:33 -0500

    different

--- different.py
+++ different.py
@@ -61,29 +61,29 @@ while True:
             disty = 10000
             id = 0
             items = os.listdir(output_folder)
-            print("items:", items)
+            # print("items:", items)
             # Check if the length of the list is zero (i.e., directory is empty)
             if len(items) != 0:
                 fi = DeepFace.find(bgr_frame, output_folder,"VGG-Face","cosine",0,"opencv",1,0,0.6,"base",0)
-                print("Face matching results:", fi)
-                print("PEEPS results:", peeps)
+                # print("Face matching results:", fi)
+                # print("PEEPS results:", peeps)
                 for df in fi:
                     identities = df['identity'].to_list()
-                    disties = df['distance'].to_list()
-                    for index, value in enumerate(disties):
-                        if value < disty:
-                            id = identities[index]
-                            if id not in peeps:
-                                peeps.append(id)
-                            k = peeps.index(id)
+                    id = identities[0]
+                    if id not in peeps:
+                        peeps.append(id)
+                    k = peeps.index(id)
+                    break
             if id == 0:
                 # Save the frame as an image in the output folder
-                output_path = os.path.join(output_folder, f"frame_{frame_number}_{x}-{y}-{w}-{h}.png")
-                print("Saving frame to:", output_path)
+                output_path = os.path.join(output_folder, f"frame_{frame_number}_{x}_{y}_{w}_{h}.png")
+                # print("Saving frame to:", output_path)
                 image = Image.fromarray(bgr_frame)
                 image.save(output_path)
+                break
         result = DeepFace.analyze(face, actions=['emotion'], enforce_detection=False)
-        col = cols[k]
+        # print("K---:",k)
+        col = cols[(int)(k / 1000)]
         if isinstance(result, list) and len(result) > 0:
             emotion = max(result[0]['emotion'].items(), key=lambda x: x[1])[0]
             cv2.rectangle(frame, (x, y), (x+w, y+h), col, 2)
